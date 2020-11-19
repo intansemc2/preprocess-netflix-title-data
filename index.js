3d0a7179c40e59dcb7f1afa308de56b27d826464
intansemc2 <intansemc2@gmail.com>
2020-11-19 16:04:27 +0700

    init project

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+//Get the source
+const sourceCSV = fs.readFileSync('./netflix_titles.csv');
+
+//Source to data
+//Define class to store source
+class csv {
+    static parse() {
+        //
+    }
+}
+
+//Preprocess
+
+//Write result
